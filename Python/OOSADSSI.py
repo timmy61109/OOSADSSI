56977a3ea9cf6b9bd13574d7486cbd01853da61f
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2018-09-23 22:30:48 +0800

    Delete .py

--- Python/OOSADSSI.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# coding = UTF-8
-#學務處生輔組站立反省(OOSADSSI,Office Of Student Affairs Discipline Section Standing Introspection)
-
-from openpyxl import *
-import time
-
