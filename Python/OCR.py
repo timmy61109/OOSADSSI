c8d24552f70a01e45c182e709b70ee5e9265715a
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2018-09-25 23:07:38 +0800

    自動取得照片名稱(Automatically get photo name)
    利用os函式來讀取資料夾中的檔案，減少人員的操作。
    Use the os function to read the files in the folder and reduce the 
    operation of the personnel.

--- Python/OCR.py
+++ Python/OCR.py
@@ -3,6 +3,10 @@
 # coding = UTF-8
 from PIL import Image
 import pytesseract
+import os
 
-text = pytesseract.image_to_string(Image.open('../Image/1.png'),lang='chi_sim')
-print text
+result_dir = '../Image/'
+lists = os.listdir(result_dir)
+for Document in lists:
+    text = pytesseract.image_to_string(Image.open('../Image/' + Document),lang='chi_sim')
+    print text
