421fc6f7eb1ee1c86e58acc8356c72175d667366
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2018-09-27 23:19:32 +0800

    將電子表格以CSV存檔(Archive your spreadsheet in CSV)
    利用逗點檔來將圖片轉為RGB以Xen格式紀錄。
    Use a comma file to convert the image to RGB and record it in Xen 
    format.

--- Python/PhotoSpreadsheets.py
+++ Python/PhotoSpreadsheets.py
@@ -7,12 +7,17 @@ import xlsxwriter
 import imageio
 import FileAccess as FA
 
-def a(ReadPath, WritePath):
+def PhotoSpreadsheets(ReadPath, WritePath):
     pict = imageio.imread(ReadPath)
     FA.CreateCSV(WritePath)
     for RGBList in pict:
+        i = 0
         for RGB in RGBList:
-            col = '#%02x%02x%02x' % (RGB[0], RGB[1], RGB[2])
-            col.append(col)
-            print col
-        print col
+            col = '%02x%02x%02x' % (RGB[0], RGB[1], RGB[2])
+            if i == 0:
+                ColList = col
+                i = 1
+            else:
+                ColList += col + ","
+        ColList += "\n"
+        FA.AddDataCSV(WritePath, ColList)
