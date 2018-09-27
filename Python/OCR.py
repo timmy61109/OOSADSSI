64f730c104ebe48e2d0471e0b604c31815d0ebc9
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2018-09-27 23:24:17 +0800

    執行PhotoSpreadsheets(Run PhotoSpreadsheets)
    一個可以跑讀取圖片到存入CSV的範例。
    An example that can run a read image to a CSV.

--- Python/OCR.py
+++ Python/OCR.py
@@ -10,3 +10,7 @@ lists = os.listdir(result_dir)
 for Document in lists:
     text = pytesseract.image_to_string(Image.open('../Image/' + Document),lang='chi_sim')
     print text
+
+import PhotoSpreadsheets as PS
+ReadPath, WritePath = "../Image/3.png", "./1.csv"
+PS.PhotoSpreadsheets(ReadPath, WritePath)
