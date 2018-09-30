9da1a25bd0bb525b2a9e88efdba514e2004ffdf5
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2018-09-30 23:06:18 +0800

    range() integer end argument expected, got tuple.
    Traceback (most recent call last):
      File "E:\Users\timmy\Documents\GitHub\OOSADSSI\Python\OCR.py", line 
    13, in <module>
        PS.PhotoRGBCSV(ReadPath, WritePath)
      File 
    "E:\Users\timmy\Documents\GitHub\OOSADSSI\Python\PhotoSpreadsheets.py", 
    line 35, in PhotoRGBCSV
        for X, Y, col in range(PhotoRGB(ReadPath)):
    TypeError: range() integer end argument expected, got tuple.

--- Python/OCR.py
+++ Python/OCR.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # coding = UTF-8
-from PIL import Image
-import pytesseract
+
 import os
+import PhotoSpreadsheets as PS
 
 result_dir = '../Image/'
 lists = os.listdir(result_dir)
 for Document in lists:
-    text = pytesseract.image_to_string(Image.open('../Image/' + Document),lang='chi_sim')
-    print text
-
-import PhotoSpreadsheets as PS
-ReadPath, WritePath = "../Image/3.png", "./1.csv"
-PS.PhotoSpreadsheets(ReadPath, WritePath)
+    ReadPath, WritePath = "../Image/" + Document, "../Data/" + Document[:-4] + ".csv"
+    #PS.PhotoSpreadsheetsCSV(ReadPath, WritePath)
+    PS.PhotoRGBCSV(ReadPath, WritePath)
--- Python/PhotoSpreadsheets.py
+++ Python/PhotoSpreadsheets.py
@@ -7,7 +7,7 @@ import xlsxwriter
 import imageio
 import FileAccess as FA
 
-def PhotoSpreadsheets(ReadPath, WritePath):
+def PhotoSpreadsheetsCSV(ReadPath, WritePath):
     pict = imageio.imread(ReadPath)
     FA.CreateCSV(WritePath)
     for RGBList in pict:
@@ -21,3 +21,32 @@ def PhotoSpreadsheets(ReadPath, WritePath):
                 ColList += col + ","
         ColList += "\n"
         FA.AddDataCSV(WritePath, ColList)
+
+def PhotoRGB(ReadPath):
+    pict = imageio.imread(ReadPath)
+    for X in range(0, pict.shape[0]):
+        for Y in range(0, pict.shape[1]):
+            a = pict[X, Y]
+            col = '%03d%03d%03d' % (a[0], a[1], a[2])
+            return X, Y, col
+
+def PhotoRGBCSV(ReadPath, WritePath):
+    FA.CreateCSV(WritePath)
+    for X, Y, col in range(PhotoRGB(ReadPath)):
+        ColList = X + "," + Y + "," + col + "\n"
+        FA.AddDataCSV(WritePath, ColList)
+
+def PhotoSpreadsheetsXlsx(ReadPath, WritePath):
+    pict = imageio.imread(ReadPath)
+
+    workbook = xlsxwriter.Workbook(WritePath)
+    worksheet = workbook.add_worksheet()
+
+    for i in range(0, pict.shape[0]):
+        for j in range(0, pict.shape[1]):
+            a = pict[i, j]
+            col = '#%02x%02x%02x' % (a[0], a[1], a[2])
+            format = workbook.add_format()
+            format.set_bg_color(col)
+            worksheet.write(i, j, '', format)
+    workbook.close()
