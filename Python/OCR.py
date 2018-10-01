7e99cd873a26f56efa7f4ccad8875c4db4ac5fa5
Wu Chenzhi <38396747+timmy61109@users.noreply.github.com>
2018-10-01 16:02:45 +0800

    暫時上傳

--- Python/OCR.py
+++ Python/OCR.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # coding = UTF-8
-
+python
 import os
 import PhotoSpreadsheets as PS
 
@@ -11,3 +11,28 @@ for Document in lists:
     ReadPath, WritePath = "../Image/" + Document, "../Data/" + Document[:-4] + ".csv"
     #PS.PhotoSpreadsheetsCSV(ReadPath, WritePath)
     PS.PhotoRGBCSV(ReadPath, WritePath)
+
+^Z
+python
+import os
+import PhotoSpreadsheets as PS
+import imageio
+import FileAccess as FA
+
+def PhotoRGB(ReadPath):
+    pict = imageio.imread(ReadPath)
+    for X in range(0, pict.shape[0]):
+        for Y in range(0, pict.shape[1]):
+            a = pict[X, Y]
+            col = '%03d%03d%03d' % (a[0], a[1], a[2])
+            col, X ,Y = str(col), str(X), str(Y)
+            #AllList = [X, Y, col]
+            AllList = X + Y + col
+            print AllList
+            #return AllList
+
+result_dir = '../Image/'
+lists = os.listdir(result_dir)
+for Document in lists:
+    ReadPath = "../Image/" + Document
+    PhotoRGB(ReadPath)
